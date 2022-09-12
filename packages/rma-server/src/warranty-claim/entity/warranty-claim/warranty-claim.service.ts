91fae0f224823320a01200a5441c8b146ea39bf0
Farid Ahmed <fnrfarid@gmail.com>
2022-09-12 17:10:51 +0500

    Fix: Filtered claims 'Delivered by' date_range issue

--- packages/rma-server/src/warranty-claim/entity/warranty-claim/warranty-claim.service.ts
+++ packages/rma-server/src/warranty-claim/entity/warranty-claim/warranty-claim.service.ts
@@ -54,9 +54,11 @@ export class WarrantyClaimService {
       Object.keys(sortQuery).length === 0 ? { modifiedOn: 'desc' } : sortQuery;
 
     if (filter_query?.fromDate && filter_query?.toDate && filter_query.date_type =='Delivery Date' ) {
+      var date = new Date(filter_query.fromDate)
+      var newDate = date.setDate(date.getDate()+1)
       dateQuery = {
         delivery_date: {
-          $gte: new Date(filter_query.fromDate).toISOString().split('T')[0],
+          $gte: new Date(newDate).toISOString().split('T')[0],
           $lte: new Date(filter_query.toDate).toISOString().split('T')[0],
         },
       };
